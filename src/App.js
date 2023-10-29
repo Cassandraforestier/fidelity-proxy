0a311b7a69588d202babac9a55f5c3f43416b153
Cassandra Forestier <cassandra.forestier@hotmail.fr>
2023-10-29 17:28:30 +0100

    all products by vendor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import "dayjs/locale/fr";
 
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import CategoriesList from './page/products-list/category-list';
 import { ConfigProvider } from "antd";
 import Connection from './page/login/connection-page';
+import DiscountPage from './page/discount/discount-page';
 import Home from "./page/home/home-page"
 import Layout from './layout';
 import Map from './page/map/map';
 import ProductsByCategory from './page/products-list/productsByCategory';
+import ProductsByVendor from './page/vendors/products-by-vendor';
 import ProductsList from './page/products-list/productsList';
 import RegistrationForm from './page/signup/registration-form-page';
 import UserProfile from './page/profile/userProfile-page';
+import VendorList from "./page/vendors/vendors-list"
 import dayjs from "dayjs";
 import frFR from "antd/lib/locale/fr_FR";
 import { useState } from 'react';
-import DiscountPage from './page/discount/discount-page';
-
 
 dayjs.locale("fr");
 const themeCustomValues = {
@@ -47,12 +48,14 @@ function App() {
             <Route index element={<Home logged={logged} />} />
             <Route path='/login' element={<Connection setLogged={setLogged} setUserType={setUserType} />} />
             <Route path='/signup' element={<RegistrationForm />} />
-            <Route path='/profile' element={<UserProfile/>}/>
-            <Route path='/discount' element={<DiscountPage/>}/>
+            <Route path='/profile' element={<UserProfile />} />
+            <Route path='/discount' element={<DiscountPage />} />
             <Route path='/profile' element={<UserProfile />} />
             <Route path='/catalog' element={<ProductsList />} />
             <Route path='/category' element={<CategoriesList />} />
             <Route path="/category/:category" element={<ProductsByCategory />} />
+            <Route path='/vendors' element={<VendorList />} />
+            <Route path="/vendors/products/:vendorId" element={<ProductsByVendor />} />
             <Route path='/map' element={<Map />} />
           </Route>
         </Routes>
